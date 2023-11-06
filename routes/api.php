cdf9f7702ca7bc5140d48f5f34105a3543fccb82
Muhammad Huraira <muhammadhuraira4@gmail.com>
2023-11-06 18:00:30 +0500

    Used 'auth:sanctum' to make the GET APIs secure

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('applicants', ApplicantApiController::class);
+
+    Route::resource('machineTypes', MachineTypeControllerApi::class);
+
+    Route::resource('finalResults', FinalResultControllerApi::class);
+
+    Route::resource('testTypes', TestTypeControllerApi::class);
+
+    Route::resource('questions', QuestionControllerApi::class);
 });
 
 // Route::post('/login', [AuthController::class, 'login']);
@@ -31,31 +43,25 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
 });
 
-Route::resource('applicants', ApplicantApiController::class);
+
 
 // Route::controller(ApplicantApiController::class)->group(function () {
 //     Route::get('applicants/{applicant}', 'show');
 // });
 
-Route::resource('machineTypes', MachineTypeControllerApi::class);
-
 // Route::controller(MachineTypeControllerApi::class)->group(function () {
 //     Route::get('machinesTypes/{machineType}', 'show');
 // });
 
-Route::resource('finalResults', FinalResultControllerApi::class);
 
 // Route::controller(FinalResultControllerApi::class)->group(function () {
 //     Route::get('finalResults/{finalResult}', 'show');
 // });
 
-Route::resource('testTypes', TestTypeControllerApi::class);
-
 // Route::controller(TestTypeControllerApi::class)->group(function () {
 //     Route::get('testTypes/{testType}', 'show');
 // });
 
-Route::resource('questions', QuestionControllerApi::class);
 
 // Route::controller(QuestionControllerApi::class)->group(function () {
 //     Route::get('questions/{question}', 'show');
